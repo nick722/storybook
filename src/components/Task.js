c84e982ecf0380ee90c167d7322d6edb3736846f
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-12-02 12:10:32 +0300

    add PropTypes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Task({
-  task: { id, title, state },
-  onArchiveTask,
-  onPinTask
-}) {
+function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -34,3 +31,15 @@ export default function Task({
     </div>
   );
 }
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func
+};
+
+export default Task;
